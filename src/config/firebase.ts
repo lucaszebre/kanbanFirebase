34f746524dae2f7d14802f665bce2b4519650d72
lucaszebre <yakirisk@gmail.com>
2023-04-04 17:30:13 +0200

    change the firebase.ts file

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,25 +1,25 @@
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAZtgWCYP_vMHzL9IoK2QQRvJOSga0GCbw",
+  authDomain: "kanbatask2.firebaseapp.com",
+  databaseURL: "https://kanbatask2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kanbatask2",
+  storageBucket: "kanbatask2.appspot.com",
+  messagingSenderId: "599370732411",
+  appId: "1:599370732411:web:551c51f92976b3e29923de"
 };
 
 // Check if the app has been initialized, and if not, initialize it.
-const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
+const app = firebase.initializeApp(firebaseConfig);
 
-const auth = app.auth();
-const db = app.firestore();
-const provider = new firebase.auth.GoogleAuthProvider();
-const firestore = db;
 
-export { auth, db, provider, firestore };
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
